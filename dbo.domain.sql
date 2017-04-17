eb6eec23b7305ff56ae2d47bc8afabe85a378e28
Pallav Shah <Pallav Shah>
2017-04-17 18:57:29 -0500

    Initial Commit

---
+++ dbo.domain.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[domain]
+(
+	[DomainId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(100) NULL, 
+    [Details] VARCHAR(5000) NULL, 
+    [Rules] VARCHAR(5000) NULL, 
+    [AgreementDocument] NCHAR(10) NULL, 
+    
+)
---
+++ dbo.officer.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[officer]
+(
+	[OfficerId] INT NOT NULL  IDENTITY, 
+    [Name] VARCHAR(100) NULL, 
+    [MobileNo] VARCHAR(50) NULL, 
+    [EmailId] VARCHAR(100) NULL, 
+    [Username] VARCHAR(100) NULL, 
+    [Password] VARCHAR(100) NULL, 
+    [AllocationTime] DATETIME NULL, 
+    [ProjectId] INT NULL, 
+    CONSTRAINT [PK_officer] PRIMARY KEY ([OfficerId])
+)
